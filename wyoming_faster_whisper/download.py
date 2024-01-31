2e5d6f4f666d17642dbf7ad70b36c011f319dd00
Neow15ard <neowisard@users.noreply.github.com>
2024-01-31 20:15:32 +0300

    Aadd LARGE V3 model

--- wyoming_faster_whisper/download.py
+++ wyoming_faster_whisper/download.py
@@ -25,6 +25,8 @@ class FasterWhisperModel(str, Enum):
     SMALL_INT8 = "small-int8"
     MEDIUM = "medium"
     MEDIUM_INT8 = "medium-int8"
+    LARGE = "large-v3"  # Downloat it https://huggingface.co/Systran/faster-whisper-large-v3/tree/main
+    #LARGE_INT8 = "" Make it from f16 ct2-transformers-converter --model /ai/models/whisper/whisper-large-v3 --output_dir large-v3i --copy_files tokenizer.json preprocessor_config.json --quantization int8_float32
 
 
 EXPECTED_HASHES = {
